2694dbdf27302edb136eef0f069f769da519d9d0
IhorKevin <ihorkevin@gmail.com>
2016-02-22 23:46:53 +0200

    frontend madness: remake speakers section

---
+++ mad_fe/dev/js/speakers.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        nav: true,
+        autoplay: true,
+        autoplayTimeout: 5000,
+        autoplayHoverPause: true,
+        items:3,
+        navText: false,
+        pagination: false
+    });
+});
