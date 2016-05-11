2394313b39e15ed531f47f5b6cf3b112effc2c07
IhorKevin <ihorkevin@gmail.com>
2016-05-11 23:31:25 +0300

    add vk init

--- shpatel/source/Post.js
+++ shpatel/source/Post.js
@@ -1,6 +1,15 @@
 import React from 'react';
 
 class Post extends React.Component {
+
+    componentDidMount() {
+        let apiUrl = 'http://api.vk.com/method/wall.get?owner_id=-111877258&count=2';
+        let xhr = new XMLHttpRequest();
+        xhr.open('GET', apiUrl);
+        xhr.send();
+    }
+
+
     render() {
         return (
             <div className="page__wrap">{this.props.post.user}: {this.props.post.text}</div>
