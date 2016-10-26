7448ffbe007e94d02e678000fcca744bd1ecb2d3
IhorKevin <ihorkevin@gmail.com>
2016-10-27 00:00:02 +0300

    delete node_modules

--- advanced/lesson1/news/js/news.js
+++ advanced/lesson1/news/js/news.js
@@ -3,6 +3,8 @@
     var url = 'http://node.dev.puzankov.com/rss/data';
     var tmplDom = $('.article_tmpl');
     var articlesDom = $('.articles');
+    var pagerDom = $('.myPager');
+    var pagerTmpl = $('.page-navigation_tmpl');
 
     function getFeed() {
         var hash = window.location.hash,
@@ -73,6 +75,51 @@
         var date = new Date(dateStr);
 
         return date;
+    }
+    renderPager(1, 8);
+     pagerDom.on('click', changePage);
+
+
+    function changePage(event) {
+        if(event.target.tagName == 'A' )
+        alert(event.target.textContent);
+    }
+
+    function renderPager(currentPage, wholeLength) {
+        var newPager = pagerTmpl.clone().removeClass('page-navigation_tmpl');
+        var pages = newPager.find('.nav-page');
+
+        if(currentPage === 1) {
+            newPager
+                .find('.nav-next')
+                .addClass('nav-next_hide');
+        } else {
+            newPager
+                .find('.nav-next')
+                .find('a')
+                .attr('href', '#');
+        }
+
+        for(var i = 0; i < 4; i++) {
+            $(pages[i])
+                .find('a')
+                .html(currentPage + i)
+                .attr('href', '#');
+        }
+
+        if(currentPage === wholeLength) {
+            newPager
+                .find('.nav-previous')
+                .addClass('nav-previous_hide');
+        } else {
+            newPager
+                .find('.nav-previous')
+                .find('a')
+                .attr('href', '#');
+        }
+
+        pagerDom.html(newPager);
+
     }
 
 })(window, jQuery);
